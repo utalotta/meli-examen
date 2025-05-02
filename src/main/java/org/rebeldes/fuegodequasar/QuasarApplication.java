8511736501539c9c48585959ade485b14f0be576
Ulises Talotta <ulises.talotta@gmail.com>
2025-05-02 00:46:41 -0300

    Primer commit

---
+++ src/main/java/org/rebeldes/fuegodequasar/QuasarApplication.java
@@ -0,0 +1,12 @@
+package org.rebeldes.fuegodequasar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuasarApplication {
+	
+    public static void main(String[] args) {
+        SpringApplication.run(QuasarApplication.class, args);
+    }
+}
